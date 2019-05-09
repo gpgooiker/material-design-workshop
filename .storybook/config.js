9cfc59810dddfbe182731d2f1cf366895e25fe8a
GP Gooiker <gp.gooiker@luminis.eu>
2019-05-09 15:24:09 +0200

    Install Material Design, vanilla Material Design stories and remove unused Storybook addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,10 @@ import { configure } from '@storybook/angular';
 // automatically import all files ending in *.stories.ts
 const req = require.context('../stories', true, /\.stories\.ts$/);
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req
+    .keys()
+    .sort()
+    .forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
---
+++ stories/select.stories.ts
@@ -0,0 +1,109 @@
+import { storiesOf } from '@storybook/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule, MatFormFieldModule } from '@angular/material';
+import { array, boolean, text, object, withKnobs } from '@storybook/addon-knobs/angular';
+import { action } from '@storybook/addon-actions';
+
+storiesOf('Select', module)
+  .addDecorator(withKnobs)
+  .add('Single Select', () => ({
+    template: `
+    <div style="background: lightgray">
+      <mat-form-field>
+        <mat-select (change)="onChange($event)" [placeholder]="placeholder" [disabled]="false">
+          <mat-option *ngFor="let food of foods" [value]="food">
+            {{food}}
+          </mat-option>
+        </mat-select>
+      </mat-form-field>
+    </div>
+    `,
+    props: {
+      foods: array('foods', [
+        'Pizza',
+        'Burgers',
+        'Steak',
+        'Tacos'
+      ]),
+      placeholder: text('placeholder', 'Favorite food'),
+      disabled: boolean('disabled', false),
+      onChange: action('change')
+    },
+    moduleMetadata: {
+      imports: [MatFormFieldModule, BrowserAnimationsModule, MatSelectModule]
+    }
+  }))
+  .add('Multi Select', () => ({
+    template: `
+    <div style="background: lightgray">
+      <mat-form-field>
+        <mat-select (change)="onChange($event)" [placeholder]="placeholder" multiple [disabled]="disabled">
+          <mat-option *ngFor="let food of foods" [value]="food">
+            {{food}}
+          </mat-option>
+        </mat-select>
+      </mat-form-field>
+    </div>
+    `,
+    props: {
+      foods: array('foods', [
+        'Pizza',
+        'Burgers',
+        'Steak',
+        'Tacos'
+      ]),
+      placeholder: text('placeholder', 'Favorite food'),
+      disabled: boolean('disabled', false),
+      onChange: action('change')
+    },
+    moduleMetadata: {
+      imports: [MatFormFieldModule, BrowserAnimationsModule, MatSelectModule]
+    }
+  }))
+  .add('Groups', () => ({
+    template: `
+    <div style="background: lightgray">
+      <mat-form-field>
+        <mat-select (change)="onChange($event)" [placeholder]="placeholder" [disabled]="disabled">
+          <mat-option>-- None --</mat-option>
+          <mat-optgroup *ngFor="let group of foodGroups" [label]="group.name">
+            <mat-option *ngFor="let food of group.foods" [value]="food">
+              {{food}}
+            </mat-option>
+          </mat-optgroup>
+        </mat-select>
+      </mat-form-field>
+    </div>
+    `,
+    props: {
+      foodGroups: object('foodGroups', [
+        {
+          name: 'Fruit',
+          foods: [
+            'Apple',
+            'Orange'
+          ]
+        },
+        {
+          name: 'Vegetables',
+          foods: [
+            'Lettuce',
+            'Broccoli'
+          ]
+        },
+        {
+          name: 'Meats',
+          foods: [
+            'Steak',
+            'Chicken'
+          ]
+        }
+      ]),
+      disabled: boolean('disabled', false),
+      placeholder: text('placeholder', 'Foods'),
+      onChange: action('change')
+    },
+    moduleMetadata: {
+      imports: [MatFormFieldModule, BrowserAnimationsModule, MatSelectModule]
+    }
+  }));
