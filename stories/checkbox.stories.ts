239e21b7496242725139b7193a6ecc6af680e388
GP Gooiker <gp.gooiker@luminis.eu>
2019-05-09 15:24:09 +0200

    Install Material Design, vanilla Material Design stories and remove unused Storybook addons

---
+++ stories/checkbox.stories.ts
@@ -0,0 +1,24 @@
+import { storiesOf } from '@storybook/angular';
+import { MatCheckboxModule } from '@angular/material';
+import { action } from '@storybook/addon-actions';
+import { boolean, text, withKnobs } from '@storybook/addon-knobs/angular';
+
+storiesOf('Checkbox', module)
+  .addDecorator(withKnobs)
+  .add('Basic', () => ({
+    template: `
+    <div>
+      <mat-checkbox [disabled]="disabled" (change)="onChange($event)">
+        {{label}}
+      </mat-checkbox>
+    </div>
+    `,
+    props: {
+      disabled: boolean('disabled', false),
+      label: text('label', 'Check me'),
+      onChange: action('change')
+    },
+    moduleMetadata: {
+      imports: [MatCheckboxModule]
+    }
+  }));
